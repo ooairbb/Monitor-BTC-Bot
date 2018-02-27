146d649a6d04d95861f3531837461d2fe7bab463
unknown <linxing301@gmail.com>
2018-02-27 16:06:09 +0800

    Change API URL

--- Monitor.py
+++ Monitor.py
@@ -1,13 +1,16 @@
 import telebot, requests, json,logging
 from config import TOKEN
 bot = telebot.TeleBot(TOKEN)
-URL_BTC = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD'
+API = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms={0}&tsyms={1}'
 
-def getprice():
-	response = requests.get(URL_BTC)
+def getPrice(DIGITAL_CURRENCY, CURRENCY):
+	URL = API.format(DIGITAL_CURRENCY, CURRENCY)
+	response = requests.get(URL)
 	out = json.loads(response.content)
-	BTC = out['BTC']
-	USD = BTC['USD']
+	BTC = out[DIGITAL_CURRENCY]
+	USD = BTC[CURRENCY]
 	return USD*6.31
 
-bot.send_message("@testmypythonbot", text = 'BTC Price :' + str(getprice()))
+#bot.send_message("@testmypythonbot", text = DIGITAL_CURRENCY + ' Price :' + str(getPrice('BTC', 'USD')))
+bot.send_message("@testmypythonbot", text = 'ETH' + ' Price :' + str(getPrice('ETH', 'USD')))
+bot.send_message("@testmypythonbot", text = 'BCC' + ' Price :' + str(getPrice('BCC', 'USD')))
