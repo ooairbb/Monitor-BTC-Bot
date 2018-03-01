00d80aafdf3a3e7fe6569aa545093e503c593ac6
unknown <linxing301@gmail.com>
2018-03-01 14:27:19 +0800

    add logging

--- jiandan.py
+++ jiandan.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import telebot, requests, json,logging, os
 from config import TOKEN
 bot = telebot.TeleBot(TOKEN)
-
+logging.basicConfig(filename='bot.log',level = logging.DEBUG)
 JIANDAN_URL = 'http://jandan.net/'
 HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.94 Safari/537.36'}
 
